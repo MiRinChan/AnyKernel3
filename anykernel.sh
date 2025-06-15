a2dcab305b6a10d451bc77466b173ecc4d97577f
米凛MiRin <148533509+MiRinChan@users.noreply.github.com>
2025-06-15 16:20:54 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string= ShirkNeko patched kernel with SukiSU+KPM
+kernel.string=SukiSU GKI kernel with susfs
 do.devicecheck=0
 do.modules=0
 do.systemless=0
