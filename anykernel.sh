37452b3054dd45e0de16faac6105bb25786f24c9
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2025-06-20 22:49:09 +0800

    copy from WildJames
    Co-authored-by: James McConnell <bins4us@hotmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -2,53 +2,32 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# global properties
+# begin properties
 properties() { '
-kernel.string=SukiSU GKI kernel with susfs
-do.devicecheck=0
+kernel.string=Sultan Kernel for the Pixel 9/Pro/Fold/XL with SUSFS Added by TheWildJames / Morgan Weedman
+do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
-device.name3=
-device.name4=
-device.name5=
-supported.versions=
-supported.patchlevels=
-supported.vendorpatchlevels=
+device.name1=caiman
+device.name2=komodo
+device.name3=tokay
+device.name4=comet
+supported.versions=15
+supported.patchlevels=2025-03 - 2025-05
 '; } # end properties
 
+# boot image installation
+block=boot;
+is_slot_device=1;
+. tools/ak3-core.sh;
+split_boot;
+flash_boot;
 
-### AnyKernel install
-## boot shell variables
-block=boot
-is_slot_device=auto
-ramdisk_compression=auto
-patch_vbmeta_flag=auto
-no_magisk_check=1
-
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh
-
-kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
-case $kernel_version in
-    5.1*) ksu_supported=true ;;
-    6.1*) ksu_supported=true ;;
-    6.6*) ksu_supported=true ;;
-    *) ksu_supported=false ;;
-esac
-
-ui_print " " "  -> ksu_supported: $ksu_supported"
-$ksu_supported || abort "  -> Non-GKI device, abort."
-
-# boot install
-if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
-    split_boot # for devices with init_boot ramdisk
-    flash_boot # for devices with init_boot ramdisk
-else
-    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-fi
-## end boot install
+# vendor_kernel_boot installation (for dtb)
+block=vendor_kernel_boot;
+is_slot_device=1;
+reset_ak;
+split_boot;
+flash_boot;
